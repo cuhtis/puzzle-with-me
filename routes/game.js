ea0d56fa745f79b48160ff7058491ca891f18632
Curtis <curtis99877@gmail.com>
2015-11-07 02:16:36 -0500

    Host game

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.redirect('/');
+  res.redirect('/host');
+});
+
+router.get('/host', function(req, res, next) {
+  res.render('host');
 });
 
 module.exports = router;
