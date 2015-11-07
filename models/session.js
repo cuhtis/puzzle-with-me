47d8d8755bbccbf6b170b2bde4d8d981776bdb6d
Curtis <curtis99877@gmail.com>
2015-11-07 01:25:21 -0500

    Session model

---
+++ models/session.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Session = new mongoose.Schema({
+  session_id: String,
+  host: String,
+  num_players: Number,
+  players: Array,
+  games: Array,
+  bet_pot: Number
+});
+
+mongoose.model('Session', Session);
+
+
+var mongoose.connect('mongodb://localhost/puzzledb');
