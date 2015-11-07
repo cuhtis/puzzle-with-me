fdaec0cd5e25f02dedb93642c4fecff667edb176
kh1550 <kristin.dramedij60@gmail.com>
2015-11-07 01:40:11 -0500

    winner/loser cssless

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,14 @@ router.get('/play', function(req, res, next) {
   res.redirect('/');
 });
 
+router.get('/result', function(req, res, next) {
+  // PSUEDOCODE
+  // if (req.session.winner === req.session.id) {
+  //   res.render('/winner', {pot: game.pot});
+  // } else {
+  //   res.render('/loser', {winner: game.winner, bet: req.session.bet});
+  // }
+  res.redirect('/');
+});
+
 module.exports = router;
