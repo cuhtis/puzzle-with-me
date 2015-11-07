a14a9ea0d4d0386b199d60c8b7121bd80fb90d13
Michelle Chen <michellechen@Michelles-MacBook-Pro.local>
2015-11-07 05:19:51 -0500

    update

--- routes/game.js
+++ routes/game.js
@@ -9,22 +9,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/create_game', function(req, res, next) {
-  var newSession = new Session({
-    host: req.body.username,
-    num_players: 1,
-    games: [],
-    bet_pot: req.body.bet
+  var newHost = new Player({
+    username: req.body.username,
+    isHost: true,
+    paid: true,
+    betAmt: req.body.bet 
   });
-  newSession.save();
 
-  res.redirect('/game/host/'+newSession.id);
+  newHost.save(function() {
+    var newSession = new Session({
+      session_id: req.body.session_id,
+      host: req.body.username,
+      num_players: 1,
+      games: [],
+      players: [newHost],
+      bet_pot: req.body.bet
+    });
+    newSession.save(function(){
+      res.redirect('/game/host/'+req.body.session_id);
+    });
+  });
+
 });
 
 router.get('/join/:session_id', function(req, res, next) {
   // Do stuff as joined player
 
   // Session id is at req.params.session_id
+
   res.render('game');
+});
+
+router.post('/join/:session_id', function(req, res, next) {
+  var newPlayer = new Player({
+      username: req.body.username,
+      isHost: false,
+      paid: true,
+      betAmt: req.body.bet 
+    });
+    
 });
 
 router.get('/host/:session_id', function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Session = mongoose.model('Session');
+var Player = mongoose.model('Player');
 
 /* GET home page. */
 router.get('/', function(req, res, next) { // change ready and unpaid
@@ -11,37 +12,51 @@ router.get('/', function(req, res, next) { // change ready and unpaid
 });
 
 router.post('/create_game', function(req, res, next) {
-  var newSession = new Session({
-    session_id: req.body.session_id,
-    host: req.body.username,
-    num_players: 1,
-    games: [],
-    bet_pot: req.body.bet
+  var newHost = new Player({
+    username: req.body.username,
+    isHost: true,
+    paid: true,
+    betAmt: req.body.bet 
   });
-  newSession.save();
 
-  res.redirect('/game/host/'+req.body.session_id);
+  newHost.save(function() {
+    var newSession = new Session({
+      session_id: req.body.session_id,
+      host: req.body.username,
+      num_players: 1,
+      games: [],
+      players: [newHost],
+      bet_pot: req.body.bet
+    });
+    newSession.save(function(){
+      res.redirect('/game/host/'+req.body.session_id);
+    });
+  });
+
 });
 
 router.get('/start', function(req, res, next) {
   res.redirect('/');
 });
 
+<<<<<<< HEAD
 router.get('/wait/:game_id', function(req, res, next) {
   res.render('wait', {game_id: game_id});
-});
+=======
 
 router.get('/rules', function(req, res, next) {
   res.render('rules');
 });
 
+<<<<<<< HEAD
 router.get('/play/:game_id', function(req, res, next) {
   res.redirect('/play');
 });
 
 router.get('/game/join/:game_id', function(req, res, next) {
   res.redirect('/wait/:game_id');
-});
+=======
+
 
 router.get('/result', function(req, res, next) {
   // PSUEDOCODE
