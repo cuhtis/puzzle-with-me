48b33eee4d415a10a85fd375f30404095c4c38e5
kh1550 <kristin.dramedij60@gmail.com>
2015-11-07 10:34:38 -0500

    fixing player bug

--- routes/game.js
+++ routes/game.js
@@ -58,6 +58,7 @@ router.post('/join', function(req, res, next) {
   });
   newPlayer.save(function() {
     Session.findOne({_id: req.body.session}, function(err, session){
+      //console.log("SESSION_ID MADE: ", req.body.session);
       if (err) res.redirect('/error?type=notfound');
       else if (session.num_players >= 4) res.redirect('/error?type=full');
       else if (session.players.map(function(e) { return e.username; }).indexOf(req.body.username) > -1) res.redirect('/game/invite/'+req.body.session+"?error=taken");
@@ -87,13 +88,39 @@ router.get('/host/:session_id', function(req, res, next) {
   // Do stuff as host
   // the session id is gotten from the var at req.params.session_id
   Session.findOne({_id: req.params.session_id}, function(err, session) {
-    if (err) res.redirect('/error?type=notfound');
+    console.log("SESSION_ID: ", req.params.session_id);
+    console.log("RESULTS: ", session);
+    if (err || session == undefined) res.redirect('/error?type=notfound');
     else {
       var copyURL = req.protocol + "://" + req.get('host') + "/game/invite/" + req.params.session_id;
-      console.log(session, req.sessionID);
-      var my_player = session.players[session.players.indexOf(req.session.username)];
-      console.log(my_player);
-      var host_player = session.players[session.players.indexOf(session.host)];
+      //console.log(session, req.sessionID);
+      console.log("SESSION: ", req.session);
+
+      // FIND INDEX OF REQ PLAYER MANUALLY
+      var my_index = -1; var cnt = 0;
+      session.players.forEach(function(player){
+        console.log("PLAYER LOOP: ", cnt, player);
+        if (player.username === req.session.username) { my_index = cnt; }
+        cnt++;
+      });
+      //var my_index = session.players.indexOf(req.session.username);
+      var my_player = session.players[my_index];
+      console.log("MY INDEX: ", my_index);
+      console.log("MY PLAYER: ", my_player);
+      
+      // FIND INDEX OF HOST PLAYER MANUALLY
+      my_index = -1; cnt = 0;
+      session.players.forEach(function(player){
+        console.log("PLAYER LOOP: ", cnt, player);
+        if (player.username === session.host) { my_index = cnt; }
+        cnt++;
+      });
+
+      console.log("MY INDEX: ", my_index);
+      var host_player = session.players[my_index];
+
+      console.log("HOST: ", host_player);
+
       var other_players = session.players.filter(function(e) { return e.username !== req.session.username; });
       res.render('wait', { title: 'Puzzle With Me', copyURL: copyURL, isHost: true, me: my_player, host: host_player, others: other_players});
     }
