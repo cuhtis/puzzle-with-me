fb4f195d25301cd3844a922d087bd00d5e8f7be8
Curtis <curtis99877@gmail.com>
2015-11-07 00:16:25 -0500

    Docs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Puzzle With Me' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/index', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
